997d2d01f0fbda6a4a14f830631d8d654c0f0ae1
nishap1225 <np5594@pleasantonusd.net>
2019-10-26 21:37:09 -0700

    Committing the Audio and Message Files, Nisha

--- application/audio.py
+++ application/audio.py
@@ -12,7 +12,7 @@ def handle_data():
     from google.cloud.speech_v1 import enums
     from google.cloud.speech_v1 import types
 
-    return request.form['projectFilepath']
+    #return request.form['projectFilepath']
     #project = request.form['projectFilepath']
     #return render_template('hacks.html')
     def sample_recognize(local_file_path):
@@ -24,6 +24,7 @@ def handle_data():
         """
 
         client = speech_v1.SpeechClient()
+        audio_channel_count = 2
 
         file_name = os.path.join(
             os.path.dirname(__file__),
@@ -34,7 +35,7 @@ def handle_data():
         language_code = "en-US"
 
         # Sample rate in Hertz of the audio data sent
-        sample_rate_hertz = 16000
+        sample_rate_hertz = 44100
 
         # Encoding of audio data sent. This sample sets this explicitly.
         # This field is optional for FLAC and WAV audio formats.
@@ -43,9 +44,10 @@ def handle_data():
             "language_code": language_code,
             "sample_rate_hertz": sample_rate_hertz,
             "encoding": encoding,
+            "audio_channel_count": audio_channel_count
         }
 
-        
+
         with io.open(file_name, "rb") as f:
             content = f.read()
         audio = {"content": content}
@@ -60,4 +62,4 @@ def handle_data():
             print(u"Transcript: {}".format(alternative.transcript))
         return alternative.transcript
     #return "Hello, World hehe!"
-    return sample_recognize('nisha_test.flac')
+    return sample_recognize('output_short.flac')
--- application/video.py
+++ application/video.py
@@ -11,22 +11,42 @@ def index():
     from google.cloud.speech import enums
     from google.cloud.speech import types
 
-    def transcribe_model_selection(speech_file, model=video):
+    def transcribe_model_selection(speech_file, model= "video"): #default
+        #storage_Uri
         """Transcribe the given audio file synchronously with
         the selected model."""
         from google.cloud import speech
+
+        file_name = os.path.join(
+            os.path.dirname(__file__),
+            'resources',
+            speech_file)
+
         client = speech.SpeechClient()
 
-        with open(speech_file, 'rb') as audio_file:
+        audio_channel_count = 2
+
+        with open(file_name, 'rb') as audio_file:
             content = audio_file.read()
 
         audio = speech.types.RecognitionAudio(content=content)
+        enable_separate_recognition_per_channel = True
 
         config = speech.types.RecognitionConfig(
             encoding=speech.enums.RecognitionConfig.AudioEncoding.FLAC,
-            sample_rate_hertz=16000,
+            sample_rate_hertz=44100,
             language_code='en-US',
-            model=model)
+            model=model,
+            audio_channel_count = audio_channel_count,
+            enable_separate_recognition_per_channel = enable_separate_recognition_per_channel
+            )
+
+        #audio = {"uri": storage_uri} long
+
+        #operation = client.long_running_recognize(config, audio) long
+
+        #print(u"Waiting for operation to complete...") long
+        #response = operation.result() long
 
         response = client.recognize(config, audio)
 
@@ -35,4 +55,6 @@ def index():
             print('-' * 20)
             print('First alternative of result {}'.format(i))
             print(u'Transcript: {}'.format(alternative.transcript))
-    return transcribe_model_selection()
+        return alternative.transcript
+
+    return transcribe_model_selection('output_short.flac')
