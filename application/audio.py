145dd25291821f7689461b3de9fcf062d90e4c1e
Laryn Qi <larynqi@airbears2-10-142-138-13.airbears2.1918.berkeley.edu>
2019-10-26 21:36:06 -0700

    HTML + Flask more

--- application/audio.py
+++ application/audio.py
@@ -2,8 +2,14 @@ from application import app_instance
 from flask import render_template, request
 
 @app_instance.route('/')
-@app_instance.route('/index')
+# def test():
+#     return '/'
+@app_instance.route('/home')
+# def home():
+#     return 'home'
 @app_instance.route('/handle_data', methods =['POST'])
+# def handle():
+#     return 'handle'
 def handle_data():
     import io
     import os
@@ -12,9 +18,15 @@ def handle_data():
     from google.cloud.speech_v1 import enums
     from google.cloud.speech_v1 import types
 
-    return request.form['projectFilepath']
+    #return request.form['projectFilepath']
     #project = request.form['projectFilepath']
-    #return render_template('hacks.html')
+    if request.method == 'POST':
+        return 'HI'
+    # if request.form.get('audiofile'):
+    #     projectpath = request.form.get('audiofile')
+    #     return sample_recognize(projectpath)
+    
+    return render_template('browser.html')
     def sample_recognize(local_file_path):
         """
         Transcribe a short audio file using synchronous speech recognition
